de68219da10faf44770d11ee0bba2ba1a36d7edb
Samson Ajulor <samsonajulor@gmail.com>
2023-10-02 09:03:53 +0100

    stripping the application

--- 01_connect/src/utils/index.js
+++ 01_connect/src/utils/index.js
@@ -1,3 +1,15 @@
+import { ethers } from "ethers";
+import { rpcUrlsMap, supportedChains } from "../constants";
+
+export const isSupportedChain = (chainId) =>
+    supportedChains.includes(Number(chainId));
+
 export const shortenAccount = (account) =>
     `${account.substring(0, 6)}...${account.substring(38)}`;
+
+export const getReadOnlyProvider = (chainId) => {
+    return new ethers.JsonRpcProvider(rpcUrlsMap[chainId]);
+};
+
+
 
